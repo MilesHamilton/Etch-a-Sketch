dd56024ec6b2fa701cd005d453199817347f5478
Camturtle <mham7714@gmail.com>
2019-05-22 16:44:25 -0400

    added fuctionality to buttons and created menu prompt

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// container
-const container = document.querySelector('#container')
-
 // grids
 function createGrid(x) {
     let div = document.createElement('div');
@@ -17,7 +14,47 @@ for (let j = 0; j>x; j++) {
     cell.style.width = '(480/x)+"px"'
     row.appendChild(cell)
 }
-div.appendChild('row')
+div.appendChild(row)
 }
-turnColor()
+turnColor();
 }
+
+// reset color
+function removeColor() {
+    document.querySelectorAll('div.cell')
+    for (i = 0; i < cells.length; i++) {
+    cells[i].classlist.remove('color')
+    }  
+}
+
+// on page load create 16x16 grid
+createGrid(16);
+
+// on mouseover turn cells gray
+function turnColor() {
+    let cells = document.querySelectorAll('div.cell')
+    for (i=0; i < cells.length; i++)
+    cells[i].addEventListener('mouseover', function() {
+        cells[i].classList.add('color')
+    })
+}
+
+// refresh button
+const refresh = document.getElementById('refresh');
+refresh.addEventListener('click', removeColor);
+
+// new grid button
+const grid = document.getElementById('new')
+grid.addEventListener('click', () => {
+    let squares = prompt("how many squares per side do you want for your new grid", '');
+    if (squares) {
+        let inner = document.getElementById('inner');
+        let container = document.getElementById('container');
+        inner.removeChild('container');
+        createGrid(squares);
+    } else if (squares === null) {
+        return;
+    }
+});
+
+
